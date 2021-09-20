0a4d56028e50dfdb19b35d3d752db02ef8f941a6
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-20 16:55:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 // Your code here
 // map like functions 
 function mapToNegativize(sourceArray){
-    newArr = []
-    for (const element of sourceArray){
-        num = element * -1
+    let oldArr = [...sourceArray]
+    let newArr = []
+  
+    for (const element of oldArr){
+   
+        let num = element * -1
         newArr.push(num)
     }
     return newArr
 }
 
 function mapToNoChange(sourceArray){
-    
+    return sourceArray
 }
 
 function mapToDouble(sourceArray){
-    
+    let oldArr = [...sourceArray]
+    let newArr = []
+  
+    for (const element of oldArr){
+   
+        let num = element * 2
+        newArr.push(num)
+    }
+    return newArr
 }
 
 function mapToSquare(sourceArray){
+    let oldArr = [...sourceArray]
+    let newArr = []
+  
+    for (const element of oldArr){
+   
+        let num = element ** 2
+        newArr.push(num)
+    }
+    return newArr
+    
+}
+
+// reduce like functions 
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    
+    let total = startingPoint
+    for (const element of sourceArray){
+         total += element
+      }
+   return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    
+}
+
+function reduceToAnyTrue(sourceArray){
     
 }

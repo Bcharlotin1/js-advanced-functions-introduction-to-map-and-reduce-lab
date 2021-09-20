506c55bb87663aa9b1b0cec0757a89cb3231e506
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-20 16:10:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Your code here
+// map like functions 
+function mapToNegativize(sourceArray){
+    newArr = []
+    for (const element of sourceArray){
+        num = element * -1
+        newArr.push(num)
+    }
+    return newArr
+}
+
+function mapToNoChange(sourceArray){
+    
+}
+
+function mapToDouble(sourceArray){
+    
+}
+
+function mapToSquare(sourceArray){
+    
+}
